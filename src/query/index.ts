8128ccd8d8b1a7755c74fbf00be0d5dbd47214d9
yax <yakeen.sewsanker@gmail.com>
2023-03-26 20:09:44 +0100

    fix: mitigate 403s
    403s are more of an issue as Github has lower rate limits for un-auth users

--- src/query/index.ts
+++ src/query/index.ts
@@ -5,6 +5,8 @@ import { getFavouriteRepos } from '../utils/storage';
 export function useInfiniteQueryTrendingReposCreatedFrom(lastNumberOfDays: number, reposPerPage: number = 100) {
   const fetch = createFetchTrendingReposCreatedFrom(lastNumberOfDays, reposPerPage);
   return useInfiniteQuery('trendingReposCreatedAfterInclusive', ({ pageParam = 1 }) => fetch(pageParam), {
+    staleTime: 1000 * 60 * 30,
+    refetchOnWindowFocus: false,
     getNextPageParam: (lastPage, allPages) => {
       const nextPage = allPages.length + 1;
       return lastPage.results.length !== 0 ? nextPage : undefined;
